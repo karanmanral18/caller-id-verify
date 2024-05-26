da1f05711e17e708b42ba1f13b66f66313c6d6a2
karan <karanmanral18@gmail.com>
2024-05-26 11:37:14 +0530

    chore: add env configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getEnvFileName } from './helpers/check-env-file';
+
+const envSuffix = !!process.env.OVERRIDE_ENV
+  ? `.${process.env.OVERRIDE_ENV}`
+  : '';
+
+const envFileName = getEnvFileName(envSuffix);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [],
+      envFilePath: envFileName,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
+  exports: [ConfigService],
 })
 export class AppModule {}
