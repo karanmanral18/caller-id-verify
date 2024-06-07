49421933c247cd870b1f64d97ed9266e6dbf2572
karan <karanmanral18@gmail.com>
2024-06-07 13:41:51 +0530

    change env port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
-  await app.listen(8000);
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+  const port = configService.get('APP_PORT');
+  await app.listen(port);
 }
 bootstrap();
